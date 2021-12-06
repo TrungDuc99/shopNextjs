81e7bd03414d4e61909c640cdbbb2a4b8370961e
TrungDuc99 <dotrungduc.bl@gmail.com>
2021-12-06 19:44:35 +0700

    update code notify

--- pages/notify.tsx
+++ pages/notify.tsx
@@ -1,6 +1,8 @@
 export const getOnesignalId = async () => {
   if (window.OneSignal) {
     const userId = await window.OneSignal.getUserId()
+    console.log(userId)
+
     return userId
   } else {
     return null
